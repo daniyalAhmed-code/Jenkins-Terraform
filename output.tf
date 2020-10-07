5baf1ebc5443f8c5e956125cf0bf02368bbc4558
Daniyal <daniyal@eurustechnologies.com>
2020-10-08 04:33:24 +0500

    testing

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "vpc-subnet" {
   value = module.vpc.private-subnets-id
 }
 
-output "Logging" {
-  value = "Hello from the other side"
+output "ec2-ip" {
+  value = module.ec2-instance.public_ip
 }
